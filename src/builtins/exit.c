3e2eeb8e0ae77c6832cd6958ebbf6703dcd45cde
gasoffknr <leo@teamkonar.com>
2025-06-01 00:54:58 +0200

    patch exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,22 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: lfirmin <lfirmin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 10:00:00 by lfirmin           #+#    #+#             */
-/*   Updated: 2025/05/17 23:32:17 by root             ###   ########.fr       */
+/*   Updated: 2025/06/01 00:54:39 by lfirmin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+#include <limits.h>
+#include <errno.h>
 
-int	is_numeric(char *str)
+static int	is_valid_number(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (!str || !str[0])
+		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
+	if (!str[i])
+		return (0);
 	while (str[i])
 	{
 		if (!ft_isdigit(str[i]))
@@ -28,31 +34,80 @@ int	is_numeric(char *str)
 	return (1);
 }
 
+static long long	safe_atoll(char *str, int *overflow)
+{
+	long long	result;
+	int			sign;
+	int			i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	*overflow = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (sign == 1)
+		{
+			if (result > (LLONG_MAX - (str[i] - '0')) / 10)
+			{
+				*overflow = 1;
+				return (LLONG_MAX);
+			}
+		}
+		else
+		{
+			if (result > (-(LLONG_MIN + (str[i] - '0'))) / 10)
+			{
+				*overflow = 1;
+				return (LLONG_MIN);
+			}
+		}
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
 int	ft_exit(t_cmd *cmd, t_shell *shell)
 {
 	int			exit_code;
 	long long	num;
+	int			overflow;
 
 	printf("exit\n");
 	exit_code = shell->exit_status;
 	if (cmd->args[1])
 	{
-		if (!is_numeric(cmd->args[1]))
+		if (!is_valid_number(cmd->args[1]))
 		{
 			ft_putstr_fd("minishell: exit: ", 2);
 			ft_putstr_fd(cmd->args[1], 2);
 			ft_putstr_fd(": numeric argument required\n", 2);
-			exit_code = 2;
+			shell->exit_status = 2;
+			shell->running = 0;
+			return (2);
 		}
-		else
+		num = safe_atoll(cmd->args[1], &overflow);
+		if (overflow)
 		{
-			num = ft_atoll(cmd->args[1]);
-			exit_code = num % 256;
-			if (cmd->args[2])
-			{
-				ft_putstr_fd("minishell: exit: too many arguments\n", 2);
-				return (1);
-			}
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(cmd->args[1], 2);
+			ft_putstr_fd(": numeric argument required\n", 2);
+			shell->exit_status = 2;
+			shell->running = 0;
+			return (2);
+		}
+		exit_code = (unsigned char)(num % 256);
+		if (cmd->args[2])
+		{
+			ft_putstr_fd("minishell: exit: too many arguments\n", 2);
+			return (1);
 		}
 	}
 	shell->exit_status = exit_code;
